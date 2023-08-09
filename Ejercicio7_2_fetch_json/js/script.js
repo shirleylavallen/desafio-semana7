c6c193032a3652150f32f6becd5ce898a2f5bfe0
lucassoy <lucascapouno@gmail.comy>
2023-08-09 16:41:33 -0300

    Fetch

--- Ejercicio7_2_fetch_json/js/script.js
+++ Ejercicio7_2_fetch_json/js/script.js
@@ -1,6 +1,3 @@
-// En este archivo no utilizamos el evento "DOMContentLoaded", ya que se colocó el atributo "defer" en la importación del script,
-// que nos soluciona el problema de los elementos no cargados del DOM. Más info => https://www.w3schools.com/tags/att_script_defer.asp
-
 const DATA_URL = "json/data.json"; // URL que contiene los datos que queremos mostrar
 
 const container = document.getElementById("container"); // "Traemos" utilizando el DOM el div de id "container" para colocar la información en él
@@ -12,10 +9,19 @@ const container = document.getElementById("container"); // "Traemos" utilizando 
  */
 function showData(dataArray) {
   // El for itera sobre los elementos del array
-  for (const item of dataArray) {
+  for (const item of dataArray.students) {
     // En la siguiente línea se utilizan "backticks" para armar el String. Más info => https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Template_literals
     container.innerHTML += `<p> ${item.name} ${item.lastname} </p>`; // Se concatena cada párrafo de la manera que queremos mostrarlo al innerHTML del contenedor
   }
 }
 
-// Escribe el código necesario para realizar el fetch al archivo con los datos y mostrar los estudiantes con la función showData
+// Utilizar fetch para obtener los datos del archivo JSON
+fetch(DATA_URL)
+  .then(response => response.json())
+  .then(dataArray => {
+    // Llamar a la función showData para mostrar los datos en el DOM
+    showData(dataArray);
+  })
+  .catch(error => {
+    console.error("Error:", error);
+  });
